0023979129a362eccfc6413bb3e11d28664f091c
Fabio Moro <fabio.moro2000@gmail.com>
2022-06-02 09:21:04 +0200

    aggiunta header e parte di main

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,57 @@
+<template>
+    <header>
+        <div class="container">
+            <nav class="header__nav">
+                <div class="logo-div">
+                    <img class="logo" src="../assets/img/dc-logo.png"/>
+                </div>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </nav>
+        </div>      
+    </header>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+
+.header {
+    &__nav { 
+            display: flex;
+            justify-content: space-between;
+
+        ul {
+            display: flex;
+            align-items: center;
+            list-style-type: none;
+
+            li {
+                margin-right: 10px;
+                font-size: 13px;
+            }
+        }
+    }
+}
+
+.container {
+    margin: auto;
+    width: 70%;
+    height: 100%;
+}
+
+.logo {
+    height: 70px;
+}
+
+</style>
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Content goes here</h1>
+    </div>
+</template>
+
+<style scoped>
+
+.container {
+    background-color: black;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    color: white;
+}
+
+</style>
