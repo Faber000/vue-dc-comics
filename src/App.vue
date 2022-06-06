29bcd54d505f67afc21e1ed6b9a170df1a9374bd
Fabio Moro <fabio.moro2000@gmail.com>
2022-06-06 14:20:45 +0200

    aggiunta hero

--- src/App.vue
+++ src/App.vue
@@ -30,4 +30,3 @@ export default {
 <style lang="scss">
 @import './assets/style/style.scss';
 </style>
-
--- src/components/BaseMainTop.vue
+++ src/components/BaseMainTop.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="container">
-        <h1>Content goes here</h1>
-    </div>
+    <section>
+        <div class="hero-container">
+
+        </div>
+        <div class="container">
+            <h1>Content goes here</h1>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
@@ -18,4 +23,10 @@ h1 {
     color: white;
 }
 
+.hero-container {
+    width: 100%;
+    height: 300px;
+    background-image: url('../assets/img/jumbotron.jpg');
+    background-size: cover;
+}
 </style>
